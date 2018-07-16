b1e8e174e753309dea9687994afb894390d93858
MatthewSH <matthewh@halfpetal.com>
2018-07-16 14:37:28 -0400

    Adding ember process.

--- src/manager/Ember.ts
+++ src/manager/Ember.ts
@@ -1,16 +1,91 @@
+import * as lockfile from "lockfile"
+import * as child from "child_process";
+import * as touch from "touch";
+import * as mkdirp from "mkdirp";
+import * as path from "path";
+import * as fs from "fs";
+import { FileManager } from "./File";
+let fileExists = require("file-exists");
 let axios = require("axios");
 
 export class Ember 
 {
     private addonManager: EmberAddon;
+    private instance: child.ChildProcess;
     public emberURL = "http://localhost:5650"
 
     constructor() {
         this.addonManager = new EmberAddon(this);
     }
 
+    /**
+     * Get the instance of the Ember addon manager.
+     * 
+     * @return {EmberAddon}
+     */
     public addon(): EmberAddon {
         return this.addonManager;
+    }
+
+    /**
+     * Start Ember and create the lock file.
+     * 
+     * @return {boolean}
+     */
+    public start(): boolean
+    {
+        if(this.running()) {
+            return true;
+        }
+
+        lockfile.lock("./deps/ember.lock", (err: Error) => {
+            if(err) {
+                console.log(err);
+                return false;
+            }
+
+            this.instance = (process.platform == "linux") ? child.spawn("./deps/Ember") : child.spawn("./deps/Ember.exe");
+            console.log("Ember Process ID: " + this.instance.pid);
+
+            return true;
+        });
+
+        return false;
+    }
+
+    /**
+     * Stop Ember and remove the lock file.
+     * 
+     * @return {boolean}
+     */
+    public stop(): boolean
+    {
+        if(!this.running()) {
+            return true;
+        }
+
+        lockfile.unlock("./deps/ember.lock", (err: Error) => {
+            if(err) {
+                console.log(err);
+                return false;
+            }
+
+            this.instance.kill();
+
+            return true;
+        });
+
+        return false;
+    }
+
+    /**
+     * Determine if Ember is running or not.
+     * 
+     * @return {boolean}
+     */
+    public running(): boolean
+    {
+        return lockfile.checkSync("./deps/ember.lock");
     }
 }
 
