964b3b46b3fad9d1e28aa899dbb024f33c0b920e
MatthewSH <MatthewSH@users.noreply.github.com>
2018-09-01 00:27:45 -0400

    Adding Bult information to config.

--- src/routes/Install.ts
+++ src/routes/Install.ts
@@ -8,6 +8,19 @@ export class Install {
 
     public static postInstall(req: express.Request, res: express.Response, next: express.NextFunction) {
         Manager.get().installer().install().then(() => {
+            if (req.body.bult !== null) {
+                let db = Manager.get().database().database();
+
+                let expiresIn = Math.floor(req.body.bult.expires_in / 1.01);
+                let expiresAt = Date.now() + expiresIn;
+
+                db.set("web.expires_at", expiresAt).write();
+                db.set("web.access_token", req.body.bult.access_token).write();
+                db.set("web.refresh_token", req.body.bult.refresh_token).write();
+            } else {
+                Manager.get().database().database().set("web", {}).write();
+            }
+
             res.json({
                 installed: true
             });
