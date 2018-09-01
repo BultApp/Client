0ac2882e01875506c0b22025ff1b67750a2e9afc
MatthewSH <MatthewSH@users.noreply.github.com>
2018-09-01 02:02:54 -0400

    Writing server mode login code.

--- src/routes/ServerMode.ts
+++ src/routes/ServerMode.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import * as bcrypt from "bcryptjs";
+import { Manager } from "../manager/Manager";
 
 export class ServerMode {
     public static getLogin(req: express.Request, res: express.Response, next: express.NextFunction) {
@@ -6,6 +8,15 @@ export class ServerMode {
     }
 
     public static postLogin(req: express.Request, res: express.Response, next: express.NextFunction) {
-        //
+        bcrypt.compare(req.body.password, Manager.get().database().config().serverMode.password)
+            .then((success) => {
+                if (success) {
+                    req.session.loggedIn = true;
+
+                    res.redirect("/");
+                } else {
+                    res.redirect("/serverLogin");
+                }
+            });
     }
 }
